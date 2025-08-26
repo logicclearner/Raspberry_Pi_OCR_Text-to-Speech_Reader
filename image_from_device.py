e354f041c2cc05909e3b526f5a9e9d6d67b965a8
Dhruv Dhariwal <148764054+logicclearner@users.noreply.github.com>
2025-08-26 14:03:23 +0530

    Create image_from_device.py

---
+++ image_from_device.py
@@ -0,0 +1,33 @@
+#this will read text from image you provide from the device 
+
+import pytesseract
+import cv2
+from PIL import Image
+# from gtts import gTTS
+import os
+import time
+language = 'en'
+import pyttsx3
+ 
+image = cv2.imread('saved-img.jpg')
+string = pytesseract.image_to_string(image)
+engine = pyttsx3.init()
+#voices = engine.getProperty('voices') 
+#engine.setProperty('voice', voices[1].id)
+rate = engine.getProperty('rate')
+print (rate)
+engine.setProperty('rate', 125) 
+engine.say("hi")
+engine.say(string)
+engine.runAndWait()
+ 
+# get the string
+#string = pytesseract.image_to_string(image)
+#string =" please add your word"
+#print(string)
+# print it
+#myobj = gTTS(text=string, lang=language, slow=False)
+#myobj.save("welcome.mp3")
+#os.system("cvlc welcome.mp3 &")
+#print(string)
+#time.sleep(1)
